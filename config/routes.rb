c4f1dac16b9b5a54f5e29a1bd95201e657b45808
masanori.takeda <karaage118@gmail.com>
2019-02-09 19:17:36 +0900

    revision 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'top' => "home#top"
-  get 'about' => "home#about"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+
+  post "posts/create" => "posts#create"
+  
+  get "/" => "home#top"
+  get "about" => "home#about"
 end
