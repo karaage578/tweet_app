d08ed8070f6d669357bfd5130c0eff15ed0ee252
masanori.takeda <karaage118@gmail.com>
2019-03-02 22:43:03 +0900

    復帰地点

--- app/jobs/ifttt_sidekiq_job.rb
+++ app/jobs/ifttt_sidekiq_job.rb
@@ -1,5 +1,3 @@
-require 'sidekiq-scheduler'
-
 class IftttSidekiqJob < ApplicationJob
   queue_as :default
 
--- config/initializers/sidekiq.rb
+++
@@ -1,9 +0,0 @@
-Sidekiq::Scheduler.dynamic = true
-
-Sidekiq.configure_server do |config|
-    config.redis = { url: ENV.fetch('REDIS_URL') }
-end
-  
-Sidekiq.configure_client do |config|
-    config.redis = { url: ENV.fetch('REDIS_URL') }
-end
