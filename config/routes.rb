9095d203f940fbbde145b4d2d8f6b204b4aa80ce
masanori.takeda <karaage118@gmail.com>
2019-03-09 10:59:34 +0900

    devise認証追加　マイグレートはこのあとやる

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ require 'sidekiq/web'
 require 'sidekiq-scheduler/web'
 
 Rails.application.routes.draw do
+  devise_for :users
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
