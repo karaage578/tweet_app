7004d315addb7e26695aeec91d59ed4c192156df
masanori.takeda <karaage118@gmail.com>
2019-04-07 18:17:15 +0900

    不要なマイグレーションファイルを削除

--- db/migrate/20190216111709_add_column_post.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnPost < ActiveRecord::Migration[5.2]
-  def up
-    add_column :posts, :title, :string
-    add_column :posts, :url, :string
-  end
-end
--- db/migrate/20190405215549_syusei2.rb
+++
@@ -1,5 +0,0 @@
-class Syusei2 < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :password, :string
-  end
-end
