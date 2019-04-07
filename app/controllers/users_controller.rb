e31b168fb422dab4be24d4cb26cc98fa8b09e670
masanori.takeda <karaage118@gmail.com>
2019-04-08 06:10:11 +0900

    機能制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+    before_action :authenticate_user, {only: [:new, :create, :index, :show, :edit, :update]}
+    before_action :forbid_login_user, {only: [:login_form, :login]}
+    before_action :ensure_correct_user, {only: [:edit, :update]}
 
     def index
         @users = User.all
@@ -73,6 +76,13 @@ class UsersController < ApplicationController
         session[:login_user_id] = nil
         flash[:notice] = "ログアウトしました"
         redirect_to("/login")
+    end
+
+    def ensure_correct_user
+        if @current_user.id != params[:id].to_i
+          flash[:notice] = "権限がありません"
+          redirect_to("/posts/index")
+        end
     end
 
   end
