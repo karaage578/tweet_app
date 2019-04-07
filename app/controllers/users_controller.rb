aec08675abe287eed89bf946b0af98c491d2f598
masanori.takeda <karaage118@gmail.com>
2019-04-08 06:40:55 +0900

    今度こそ完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
- #   before_action :authenticate_user, {only: [:new, :create, :index, :show, :edit, :update]}
- #   before_action :forbid_login_user, {only: [:login_form, :login]}
- #   before_action :ensure_correct_user, {only: [:edit, :update]}
+    before_action :authenticate_user, {only: [:new, :create, :index, :show, :edit, :update]}
+    before_action :forbid_login_user, {only: [:login_form, :login]}
+    before_action :ensure_correct_user, {only: [:edit, :update]}
 
     def index
         @users = User.all
