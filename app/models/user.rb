1c0d786f73ee10ceafd4aae1ec140469022f33d5
masanori.takeda <karaage118@gmail.com>
2019-04-08 04:47:14 +0900

    ログイン機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     validates :user_id, {presence: true}
-    validates :email, {presence: true, uniqueness: true}
+    validates :email, {presence: true}
     validates :password, {presence: true}
 end
